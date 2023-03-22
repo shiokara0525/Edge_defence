29c8c64a266fca81756fae808ef1245d6c37a4ee
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-03-22 14:04:21 +0900

    ver 1_1_3 比例制御完成させたよ

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,8 @@
 
 int A = 0;  //どのチャプターに移動するかを決める変数
 
-int A_ball = 0;  //ライン踏んでるか踏んでないか
-int B_ball = 999;  //前回踏んでるか踏んでないか
-
-int A_line = 0;
-int B_line = 999;
+int A_line = 0;  //ライン踏んでるか踏んでないか
+int B_line = 999;  //前回踏んでるか踏んでないか
 
 //上二つの変数を上手い感じにこねくり回して最初に踏んだラインの位置を記録するよ(このやり方は部長に教えてもらったよ)
 
@@ -31,8 +28,6 @@ const int Tact_Switch = 15;  //スイッチのピン番号
 const int pingPin = 32;
 const double pi = 3.1415926535897932384;  //円周率
 
-int Far = 100;
-
 void Switch(int);
 int readUS();
 
@@ -40,13 +35,14 @@ Ball ball;  //ボールのオブジェクトだよ(基本的にボールの位�
 AC ac;      //姿勢制御のオブジェクトだよ(基本的に姿勢制御は全部ここ)
 LINE line;  //ラインのオブジェクトだよ(基本的にラインの判定は全部ここ)
 timer Timer;
-timer Sentor;
 moter MOTER;
+MA US;
 
 
 /*--------------------------------------------------------------モーター制御---------------------------------------------------------------*/
 
-const double val_max = 100;         //モーターの出力の最大値
+
+const double val_max = 120;         //モーターの出力の最大値
 
 /*------------------------------------------------------実際に動くやつら-------------------------------------------------------------------*/
 
@@ -70,31 +66,27 @@ void loop(){
   double AC_val = 100;  //姿勢制御の最終的な値を入れるグローバル変数
   angle go_ang(0,true);
   int goval = val_max;
-  int ball_flag = 0;  //ボールがコート上にあるかないか 
-  int Line_flag = 0;
   int stop_flag = 5;
 
   if(A == 10){  //情報入手
-    ball_flag = ball.getBallposition();  //ボールの位置取得
+    ball.getBallposition();  //ボールの位置取得
     AC_val = ac.getAC_val();             //姿勢制御の値入手
-    Line_flag = line.getLINE_Vec();      //ライン踏んでるか踏んでないかを判定
-    if(Line_flag == 0){  //ボール見てなかったら
-      A = 30;  //止まるとこ
-    }
-    else{  //ボール見てたら
-      A = 20;  //進む角度決めるとこ
-    }
+    line.getLINE_Vec();      //ライン踏んでるか踏んでないかを判定
+    A = 20;
   }
 
   if(A == 15){
-    MOTER.moter_0();
-    while(1){
-      ball.getBallposition();
-      if(ball.far_x != 0 || ball.far_y != 0){
-        break;
+    int far = readUS();
+    Serial.print(" 距離 : ");
+    Serial.print(far);
+    if(40 < far){
+      go_ang = 179.9;
+      while(line.getLINE_Vec() == 0){
+        AC_val = ac.getAC_val();
+        //MOTER.moveMoter(go_ang,goval,AC_val,0,line);
       }
     }
-    A = 50;
+    A = 20;
   }
 
   if(A == 20){
@@ -125,87 +117,21 @@ void loop(){
 
     for(int i = 0; i < 2; i++){
       if((go_border[i] - stop_range[0] < ball.ang && ball.ang < go_border[i] + stop_range[0])){
+        goval = 0;
         stop_flag = 999;
-        break;
       }
       else if(go_border[i] - stop_range[1] < ball.ang && ball.ang < go_border[i] + stop_range[1]){
-        goval = abs((abs(ball.ang) - stop_range[0]) * goval_a);
-        break;
+        goval = (stop_range[1] - abs(ball.ang)) * goval_a;
       }
     }
     if(170 < abs(go_ang.degrees)){
       stop_flag = 999;
     }
 
-    if(abs(ball.ang) < 5){
-      A_ball = 0;
-      if(A_ball != B_ball){
-        B_ball = A_ball;
-        Sentor.reset();
-      }
-    }
-    else{
-      A_ball = 1;
-      if(A_ball != B_ball){
-        B_ball = A_ball;
-        Sentor.reset();
-      }
-      Sentor.reset();
-    }
-
     A = 50;
-
-    if(1500 < Sentor.read_ms()){
-      Serial.print(" cat ");
-      stop_flag = 0;
-      go_ang = ball.ang;
-      A = 30;
-    }
   }
-  if(A == 30){
-    goval = val_max;
-    Timer.reset();
-    while(Timer.read_ms() < 500){
-      AC_val = ac.getAC_val();
-      MOTER.moveMoter(go_ang,goval,AC_val,0,line);
-    }
-    while(1){
-      ball.getBallposition();
-      AC_val = ac.getAC_val();
-      int Line_flag = line.getLINE_Vec();
-      double ang_defference = 75.0 / ball.far;  //どれくらい急に回り込みするか(ボールが近くにあるほど急に回り込みする)
-      angle linedir(line.Lvec_Dir,true);
-        
-      if(ball.ang < 0){  //ここで進む角度決めてるよ!(ボールの角度が負の場合)
-        go_ang = ball.ang + (abs(ball.ang)<90 ? ball.ang*0.5 : -45) * (0.2 + ang_defference);  //ボールの角度と距離から回り込む角度算出してるよ!
-      }
-      else{  //(ボールの角度が正の場合)
-        go_ang = ball.ang + (abs(ball.ang)<90 ? ball.ang*0.5 : 45) * (0.2 + ang_defference);  //ボールの角度と距離から回り込む角度算出してるよ!
-      }
 
-      if(Line_flag == 1){
-        line_flag = line.switchLineflag(linedir);
-
-        if(line_flag == 3){
-          break;
-        }
-        else if(line_flag == 1){
-          go_ang = 180;
-        }
-      }
-      go_ang.to_range(180,true);
-
-      Serial.print(" 進む角度 : ");
-      Serial.print(go_ang.degrees);
-      ball.print();
-      Serial.println();
-      MOTER.moveMoter(go_ang,goval,AC_val,0,line);
-    }
-    A = 10;
-  }
   if(A == 50){
-    // Serial.print(" 進む角度 : ");
-    // Serial.print(go_ang.degrees);
     Serial.print(" 進む角度 : ");
     Serial.print(go_ang.degrees);
     Serial.print(" 進む速さ : ");
@@ -224,40 +150,6 @@ void loop(){
 /*----------------------------------------------------------------いろいろ関数-----------------------------------------------------------*/
 
 
-int readUS(){
-  unsigned long duration;
-  int cm;
-  //ピンをOUTPUTに設定（パルス送信のため）
-  pinMode(pingPin, OUTPUT);
-  //LOWパルスを送信
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);  
-  //HIGHパルスを送信
-  digitalWrite(pingPin, HIGH);  
-  //5uSパルスを送信してPingSensorを起動
-  delayMicroseconds(5); 
-  digitalWrite(pingPin, LOW); 
-  
-  //入力パルスを読み取るためにデジタルピンをINPUTに変更（シグナルピンを入力に切り替え）
-  pinMode(pingPin, INPUT);
-  //入力パルスの長さを測定
-  duration = pulseIn(pingPin, HIGH,6000);
-  Serial.print(duration);
-
-  //パルスの長さを半分に分
-  duration=duration/2;
-  //cmに変換
-  cm = int(duration/29); 
-  if(cm == 0){
-    cm = 100;
-  }
-
-  delayMicroseconds(100);
-  return cm;
-}
-
-
-
 
 
 
@@ -266,14 +158,11 @@ void Switch(int flag){
   while(1){
     if(A == 0){
       if(flag == 2){
-        delay(100);
         if(digitalRead(Tact_Switch) == HIGH){
+          delay(100);
           digitalWrite(line.LINE_light,LOW);  //ラインの光止めるよ
           MOTER.moter_0();
           A = 1;
-        }
-        else{
-          break;
         }
       }
       else{
@@ -282,6 +171,7 @@ void Switch(int flag){
     }
 
     if(A == 1){
+      delay(100);
       if(digitalRead(Tact_Switch) == LOW){
         A = 2;
       }
@@ -317,4 +207,47 @@ void Switch(int flag){
     }
   }
   return;
+}
+
+
+
+
+int readUS(){
+  unsigned long duration;
+  int cm;
+  //ピンをOUTPUTに設定（パルス送信のため）
+  pinMode(pingPin, OUTPUT);
+  //LOWパルスを送信
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);  
+  //HIGHパルスを送信
+  digitalWrite(pingPin, HIGH);  
+  //5uSパルスを送信してPingSensorを起動
+  delayMicroseconds(5); 
+  digitalWrite(pingPin, LOW); 
+  
+  //入力パルスを読み取るためにデジタルピンをINPUTに変更（シグナルピンを入力に切り替え）
+  pinMode(pingPin, INPUT);
+  //入力パルスの長さを測定
+  duration = pulseIn(pingPin, HIGH,6000);
+  Serial.print(duration);
+
+  //パルスの長さを半分に分
+  duration=duration/2;
+  //cmに変換
+  cm = int(duration/29); 
+  if(cm == 0){
+    cm = 100;
+  }
+
+
+  delayMicroseconds(100);
+  cm = US.demandAve(cm);
+  for(int i = 0; i < 8; i++){
+    if(cm < 20 + (i * 10)){
+      cm = 20 + (i * 10);
+      break;
+    }
+  }
+  return cm;
 }
