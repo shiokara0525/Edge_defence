2a7804f7e6a2d01efec29d72389d694f1e14cbf9
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-03-25 10:57:50 +0900

    ver 2_1_4 うまくいったよ

--- src/main.cpp
+++ src/main.cpp
@@ -120,15 +120,15 @@ void loop(){
 
   if(A == 15){
     if(OutB_flag == 0){
-      go_ang = -150.0;
+      go_ang = -165.0;
     }
     else if(OutB_flag == 1){
-      go_ang = 150.0;
+      go_ang = 165.0;
     }
     else{
       go_ang = 179.9;
     }
-    go_ang = 179.9;
+
     goval = 100;
     while(1){
       int ac_val = ac.getAC_val();
@@ -195,12 +195,13 @@ void loop(){
     }
 
     if(120 < abs(go_ang.degree)){
-      goval /= 2;
-      MOTER.line_val = 1.5;
+      goval = 50;
+      MOTER.line_val = 2;
     }
     else{
-      MOTER.line_val = 0.35;
+      MOTER.line_val = 0.15;
     }
+    A = 50;
 
     if(abs(ball.ang) < 15){
       A_sentor = 1;
@@ -220,23 +221,38 @@ void loop(){
         Timer_sentor.reset();
       }
     }
-    A = 50;
+    
   }
 
-  if(A == 30){
+  if(A == 40){
     timer Timer_cat;
     Timer_cat.reset();
-    while(abs(ball.ang) < 15){
-      ball.getBallposition();
-      line.getLINE_Vec();
-      go_ang = ball.ang;
-      float ac_val = ac.getAC_val();
-      
-      angle linedir(line.Lvec_Dir,true);
-      MOTER.moveMoter(go_ang,goval,ac_val,0,line);
+    goval = 160;
+    if(abs(ball.ang) < 15){
+      timer Timer_dog;
+      Timer_dog.reset();
+      go_ang = 0;
+      while(Timer_dog.read_ms() < 500){
+        float ac_val = ac.getAC_val();
+        MOTER.moveMoter(go_ang,goval,ac_val,0,line);
+      }
 
-      if(1000 < Timer_cat.read_ms()){
-        break;
+      while(abs(ball.ang) < 30){
+        ball.getBallposition();
+        int line_flag = line.getLINE_Vec();
+        go_ang = ball.ang;
+        float ac_val = ac.getAC_val();
+        MOTER.moveMoter(go_ang,goval,ac_val,0,line);
+        angle linedir(line.Lvec_Dir,true);
+
+        Serial.print(" dog ");
+        ball.print();
+        Serial.println();
+
+        if(5000 < Timer_cat.read_ms() || line_flag == 1){
+          Serial.print(" ハムスター ");
+          break;
+        }
       }
     }
     A = 15;
